98b2a4abb03f31f0499042610a9f44643550e639
Pooja Singh <poojasingh82294@gmail.com>
2024-08-03 21:40:57 +0530

    Add soln for finding sum of divisors

---
+++ Day_4_Sum of Divisors.cpp
@@ -0,0 +1,31 @@
+/*
+Given a positive integer N., The task is to find the value of Σi from 1 to N F(i) where function F(i) for the number i is defined as the sum of all divisors of i.
+
+Example 1:
+
+Input:
+N = 4
+Output:
+15
+Explanation:
+F(1) = 1
+F(2) = 1 + 2 = 3
+F(3) = 1 + 3 = 4
+F(4) = 1 + 2 + 4 = 7
+ans = F(1) + F(2) + F(3) + F(4)
+    = 1 + 3 + 4 + 7
+    = 15
+
+*/
+
+class Solution
+{
+public:
+    long long sumOfDivisors(int N)
+    {
+        long long sum = 0;
+        for (int i = 1; i <= N; ++i)
+            sum += (N / i) * i;
+        return sum;
+    }
+};
