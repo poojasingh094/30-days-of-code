ae3472082bd40244d4ed252a833d6a9ecdffbd17
Pooja Singh <poojasingh82294@gmail.com>
2024-08-06 23:45:27 +0530

    Add soln to find frequency of limited range array elements

---
+++ Day_7_Frequency of Elements.cpp
@@ -0,0 +1,40 @@
+
+/*
+You are given an array arr[] containing n positive integers. These integers can be from 1 to p, and some numbers may be repeated or absent. Your task is to count the frequency of all numbers that lie in the range 1 to n.
+Do modify the array in-place changes in arr[], such that arr[i] = frequency(i) and assume 1-based indexing.
+
+Note: The elements greater than n in the array can be ignored for counting. Read the explanation of examples for better clarification.
+
+Examples
+
+Input: n = 5 arr[] = {2, 3, 2, 3, 5} p = 5
+Output: 0 2 2 0 1
+Explanation: Counting frequencies of each array element We have: 1 occurring 0 times. 2 occurring 2 times. 3 occurring 2 times. 4 occurring 0 times. 5 occurring 1 time.
+Input: n = 4  arr[] = {3,3,3,3} p = 3
+Output: 0 0 4 0
+Explanation: Counting frequencies of each array element We have: 1 occurring 0 times. 2 occurring 0 times. 3 occurring 4 times. 4 occurring 0 times.
+
+*/
+
+class Solution{
+    public:
+    //Function to count the frequency of all elements from 1 to N in the array.
+    void frequencyCount(vector<int>& arr,int N, int P)
+    { 
+          for (int i = 0; i < N; ++i) {
+            if (arr[i] > N) {
+                arr[i] = 0;
+            }
+        }
+         for (int i = 0; i < N; ++i) {
+            if (arr[i] % (N + 1) > 0) {
+                int index = (arr[i] % (N + 1)) - 1;
+                arr[index] += (N + 1);
+            }
+        }
+         for (int i = 0; i < N; ++i) {
+            arr[i] = arr[i] / (N + 1);
+        }
+      
+    }
+};
